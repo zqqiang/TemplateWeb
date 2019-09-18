41ff53d617623f4e090f3c9f8a3c48551a4db9bb
zqqiang <qiangzhaoqing@gmail.com>
2019-09-17 21:35:42 -0700

    add version

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { AppBar, Toolbar } from "@material-ui/core";
+import { Dropdown, DropdownMenu, DropdownToggle } from "reactstrap";
 
 class Header extends React.Component {
+  Apps = versions => {
+    return (
+      <ul className="jr-list jr-list-half">
+        {versions &&
+          versions.map(version => (
+            <li key={version} className="jr-list-item">
+              <Link className="jr-list-link" to="/app/calendar/basic">
+                <i className="zmdi zmdi-calendar zmdi-hc-fw" />
+                <span className="jr-list-text">
+                  <span>{version}</span>
+                </span>
+              </Link>
+            </li>
+          ))}
+      </ul>
+    );
+  };
   render() {
     return (
       <AppBar className="app-main-header">
@@ -14,6 +32,23 @@ class Header extends React.Component {
               title="Jambo"
             />
           </Link>
+          <ul className="header-notifications list-inline ml-auto">
+            <li className="list-inline-item">
+              <Dropdown className="quick-menu app-notification" isOpen={true}>
+                <DropdownToggle
+                  className="d-inline-block"
+                  tag="span"
+                  data-toggle="dropdown"
+                >
+                  <span className="app-notification-menu">
+                    <i className="zmdi zmdi-apps zmdi-hc-fw zmdi-hc-lg" />
+                    <span>Apps</span>
+                  </span>
+                </DropdownToggle>
+                <DropdownMenu>{this.Apps([`6.2.0`, `6.1.0`])}</DropdownMenu>
+              </Dropdown>
+            </li>
+          </ul>
         </Toolbar>
       </AppBar>
     );
