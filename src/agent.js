fc85073b30fe42319e027c7568fe0e298f72f261
zqqiang <qiangzhaoqing@gmail.com>
2019-10-01 21:28:11 -0700

    add json-server

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from "superagent";
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = "";
+const API_ROOT = "http://localhost:8080/api";
 
 // const encode = encodeURIComponent;
 const responseBody = res => res.body;
